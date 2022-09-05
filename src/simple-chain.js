7a34685522c47a2caf6e514e9f7fed7b9b744ba2
DrobyshSV <austin@tut.by>
2022-09-05 22:55:07 +0300

    feat: add "Chain maker" js code

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,32 +1,49 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
 
 /**
  * Implement chainMaker object according to task description
- * 
+ *
  */
 const chainMaker = {
-  getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    chain: [],
+    getLength() {
+        return this.chain.length;
+    },
+    addLink(value) {
+        if (value === undefined) {
+            this.chain.push("");
+        } else {
+            this.chain.push(`${value}`);
+        }
+        return this;
+    },
+    removeLink(position) {
+        if (
+            this.chain.length === 0 ||
+            position < 1 ||
+            position >= this.getLength() ||
+            typeof position !== "number" ||
+            !Number.isInteger(position)
+        ) {
+            this.chain = [];
+            throw new Error("You can't remove incorrect link!");
+        }
+        let partBeforeLink = this.chain.slice(0, position - 1);
+        let partAfterLink = this.chain.slice(position, this.getLength());
+        this.chain = [...partBeforeLink, ...partAfterLink];
+        return this;
+    },
+    reverseChain() {
+        this.chain = this.chain.reverse();
+        return this;
+    },
+    finishChain() {
+        let finish = "( " + this.chain.join(" )~~( ") + " )";
+        this.chain = [];
+        return finish;
+    },
 };
 
 module.exports = {
-  chainMaker
+    chainMaker
 };
