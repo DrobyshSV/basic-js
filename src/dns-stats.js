00923a3bb4935a51fdd683a9014fcea728b80fc2
DrobyshSV <austin@tut.by>
2022-09-06 16:00:48 +0300

    feat: add "DNS stat" js code

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats( domains ) {
+ let newArr = domains.map(t=> {
+     t = t.split('.');
+     let arr = [];
+     for (let i = 0; i < t.length; i++){
+         arr.push('.' + t.slice(i).reverse().join('.'));
+     }
+     return arr;
+ }).flat().sort();
+    let obj = {};
+    newArr.map(t=> {
+        obj.hasOwnProperty(t) ? obj[t]++ : obj[t] = 1
+    })
+    return obj
 }
 
 module.exports = {
